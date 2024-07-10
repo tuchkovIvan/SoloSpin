495e787c7f6b7196bfbc9dd34b08967f855df3ea
Tuchkov Ivan <tuchkov.ivan42@gmail.com>
2024-07-10 15:14:42 +0300

    feat: Aded logic for Grid construction

--- app/src/main/java/com/example/solospin/MainActivity.kt
+++ app/src/main/java/com/example/solospin/MainActivity.kt
@@ -6,38 +6,44 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
+import com.example.solospin.ui.components.ExerciseGrid
 import com.example.solospin.ui.theme.SoloSpinTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
+    lateinit var database: SoloSpinDatabase
+    lateinit var repository: SoloSpinRepository
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Voiceover.getInstance(this)
+        database = Room.databaseBuilder(applicationContext, SoloSpinDatabase::class.java, "solospin_database").build()
+        repository = SoloSpinRepository(database.techniqueDao(), database.tipDao(), database.initialTipDao())
+
+        CoroutineScope(Dispatchers.IO).launch {
+            initializeDatabase(repository = repository)
+        }
+
         setContent {
             SoloSpinTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ExerciseGrid(repository)
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SoloSpinTheme {
-        Greeting("Android")
+    override fun onDestroy() {
+        Voiceover.shutdown()
+        super.onDestroy()
     }
 }
